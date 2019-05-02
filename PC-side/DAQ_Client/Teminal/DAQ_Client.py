9b696e0b78981b01cc1032ae3bc7ae47021fe984
RadimPavlik <xpavli74@vutbr.cz>
2019-05-02 16:21:32 +0200

    Working scale on time axis. Introduced new variable TimeConversionCoefficient that represent timespan between 2samples.

--- PC-side/DAQ_Client/Teminal/DAQ_Client.py
+++ PC-side/DAQ_Client/Teminal/DAQ_Client.py
@@ -17,9 +17,6 @@ def exit_handler():
 	print("Application is ending!")
 
     
-  
-    
-
 atexit.register(exit_handler)
 
 
@@ -29,11 +26,6 @@ BUFFER_SIZE = 65536
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-
-
-#PreTrigger = 2
-#ForcedTrigger = 0
-#ChannelSelect = 0 #0=CH1, 1=CH2
 
 time_old = 0
 time_new = 0
@@ -46,20 +38,19 @@ requested = False
 VoltageConversionCoefficient = 8 # (14bit sign Range/2)/mV .. (16384/2)/1000 # = 1mV cca 8,192 float :/
 VoltagePlotOffset_mV = 0 # calibration of non symetricity
 
-TimeConversionCoefficient = 0.000067 # = 1ns
+TimeConversionCoefficient = 0.008 #uS => 1sample (RP 125 Msps => 1sample = (1/125E6)= 8E-9 Sec = 8nS)
 
 
 def Setup():
 	global VoltageConversionCoefficient
-
-	print("App in Setup")
 
 	TriggerLvl = -500 #mV
 	TriggerLvL_converted = int(TriggerLvl*VoltageConversionCoefficient)
 	print("Trigger Level converted=",TriggerLvL_converted," Trigger Level wanted=",TriggerLvl,"mV")
 	
 	PreTrigger = 2 # samples
-	ForcedTrigger = 0
+
+	ForcedTrigger = 0 # 0=Disabled, 1=Enabed
 	ChannelSelect = 1 #0=CH1, 1=CH2
 
 	TriggerLvL_converted = struct.pack('<h',TriggerLvL_converted)
@@ -67,7 +58,6 @@ def Setup():
 	Trig = b"".join([TriggerLvL_converted, TriggerFlag])
 	s.send(Trig)
 
-	#s.send(struct.pack('<i', 1<<29 | TriggerLvL_converted ))   # signed int is 'i'
 	s.send(struct.pack('<I', 2<<29 | PreTrigger ))   # number of samples
 	s.send(struct.pack('<I', 4<<29 | ForcedTrigger)) # if there is necessary to manualy trigger event
 	s.send(struct.pack('<I', 5<<29 | ChannelSelect )) # select the channel
@@ -88,6 +78,7 @@ def Receive():
 	global hodnoty
 	global VoltageConversionCoefficient
 	global VoltagePlotOffset_mV
+	global TimeConversionCoefficient
 
 	data = s.recv(BUFFER_SIZE)
 	
@@ -109,17 +100,21 @@ def Receive():
 			hodnoty = numpy.fromstring(data, dtype=numpy.int16)
 			print(hodnoty)
 			hodnoty = (hodnoty/VoltageConversionCoefficient) + VoltagePlotOffset_mV # rescale and offset of received values
-			#print(hodnoty)
+			print("Pocet hodnot prijatych",hodnoty.size)
+			casy_x = numpy.arange(0,(TimeConversionCoefficient*hodnoty.size),TimeConversionCoefficient)
+			#numpy.insert(casy_x,0)
+			print("Pocet casu generovanych prijatych",casy_x.size)
+			print(casy_x)
 			#tisk hodnoty/v grafu
-			plt.plot(hodnoty)
+			plt.plot(casy_x,hodnoty)
 			xmin=0
-			xmax=1000
-			ymin=-1000
-			ymax=1000
+			xmax=10 #uS
+			ymin=-1000 #mV
+			ymax=1000 #mV
 			plt.xlim([xmin,xmax])
 			plt.ylim([ymin,ymax])
 			plt.ylabel('Voltage [mV]')
-			plt.xlabel('Number of Samples')
+			plt.xlabel('Time [uS]')
 			plt.show()
 			print("Time before two data blocks received : " +str(time_new - time_old) )
 			print("END")
